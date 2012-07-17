02d07e274886ccc007c3cff29b2628fd5d66f437
phil1271 <phil1271>
2012-07-17 18:58:52 +0000

    -neue Reportklasse für Invoice

---
+++ chunks.artiver/src/eu/gymnaila/chunks/artiver/reports/InvoiceReportTemplate.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.gymnaila.chunks.artiver.reports;
+
+/**
+ *
+ * @author Philipp
+ */
+public class InvoiceReportTemplate 
+{
+    
+    
+    
+}
